ec3658019f152c9ae2d31016097a397e9756d791
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-16 12:21:09 +0530

    initial commit

---
+++ Models/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeChecker.Models
+{
+    internal class Player
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+
+        static int DEFAULT_AGE = 18;
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Age = DEFAULT_AGE;
+        }
+
+        public Player(int id , string name , int age):this(id,name)
+        {
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("The player with the maximum age is:");
+            return $"Player ID : {Id}\n" +
+                $"Player Name : {Name}\n" +
+                $"Player Age : {Age}";
+        }
+
+        public static Player WhoIsElder(Player[] players)
+        {
+            int maxAge = 0;
+            Player tempObject = null;
+           foreach(Player player in players)
+            {
+                if (player.Age > maxAge)
+                {
+                    maxAge = player.Age;
+                    tempObject = player;
+                }
+            }
+
+            return tempObject;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using AgeChecker.Models;
+
+namespace AgeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[] {
+
+            new Player(100,"Adam",20),
+            new Player(101,"Ajesh"),
+            new Player(102,"Damian",38),
+            new Player(103,"Arlo",29),
+            new Player(104,"James",36),
+
+            };
+
+            Player maxPlayer = Player.WhoIsElder(players);
+
+            Console.WriteLine(maxPlayer);
+        }
+    }
+}
